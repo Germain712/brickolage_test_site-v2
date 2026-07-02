b8167e1859457cb1b411458a5eba9689f3b6bb6b
Reuben <Germain712@outlook.com>
2026-07-02 20:56:19 +0100

    added new service page and also light/dark toogle

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const navToggle = document.querySelector(".v2-nav-toggle");
 const nav = document.querySelector(".v2-nav");
+const themeToggle = document.querySelector(".v2-theme-toggle");
 
+// NAV MENU
 if (navToggle && nav) {
   navToggle.addEventListener("click", () => {
     const isOpen = nav.classList.toggle("open");
@@ -9,16 +11,21 @@ if (navToggle && nav) {
   });
 }
 
-// Close menu when clicking a link (mobile)
+// Close menu when clicking a link
 document.querySelectorAll(".v2-nav a").forEach((link) => {
   link.addEventListener("click", () => {
-    if (nav.classList.contains("open")) {
-      nav.classList.remove("open");
-      navToggle.classList.remove("open");
-      navToggle.setAttribute("aria-expanded", "false");
-    }
+    nav.classList.remove("open");
+    navToggle.classList.remove("open");
+    navToggle.setAttribute("aria-expanded", "false");
   });
 });
+
+// THEME TOGGLE
+if (themeToggle) {
+  themeToggle.addEventListener("click", () => {
+    document.body.classList.toggle("theme-light");
+  });
+}
 
 // Footer year
 document.getElementById("year").textContent = new Date().getFullYear();
